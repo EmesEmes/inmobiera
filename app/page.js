a7cf2c2119c4d55a8d743bac611755b4657b54ae
Emilio Esteban <delhierroemilio@gmail.com>
2026-03-12 11:25:50 -0500

    agregado propiedades vendidas

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ function HomeContent() {
       <Hero />
       <PropiedadesDestacadas />
       <QuienesSomos />
-      {/* <PropiedadesVendidas />  */}
+      <PropiedadesVendidas />
       {/* Footer */}
       <footer className="bg-gray-900 text-white py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
